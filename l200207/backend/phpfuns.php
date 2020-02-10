6edd43ec77e0760205da173b7a59e934c05089ad
Madhuri@EIS LABS Pvt Ltd <madhuri@edeitic.com>
2020-02-10 10:03:45 +0530

    code commit

--- l200207/backend/phpfuns.php
+++ l200207/backend/phpfuns.php
@@ -15,13 +15,13 @@ if ($mode === "LOGIN") {
   session_start();
   $link = mysqli_connect('db', 'root', 'ic@0001');
   $data['email'] = strtolower($data['email']);
-  if ($data["email"] === "demo@edeitic.com"){
-    if ($data["password"] !== "demo@edeitic.com") exit('failure'.'pelase enter the right password: demo@edeitic.com');
-    $usrID = "".$timeNow.str_replace('.', '', getCLIP());
+  if ($data["email"] === "demo@edeitic.com") {
+    if ($data["password"] !== "demo@edeitic.com") exit('failure' . 'pelase enter the right password: demo@edeitic.com');
+    $usrID = "" . $timeNow . str_replace('.', '', getCLIP());
     // $usrPSS= strtoupper(md5($usrID));
     $DBnm = "eis_hrms_$usrID";
     $_SESSION["demoUsrID"] = $usrID;
-    $_SESSION["user"] = array('prid' => 1560156094488 );
+    $_SESSION["user"] = array('prid' => 1560156094488);
     $_SESSION["DBnm"] = $DBnm;
     // print_r("\nDBnm : $DBnm\n</br>");
     // print_r("\nsession_save_path : ", session_save_path());
@@ -32,7 +32,7 @@ if ($mode === "LOGIN") {
     if ($stmtCheckTableList->error) exit(' Failure');
     $stmtCheckTableListR = $stmtCheckTableList->get_result();
     $createDBQ = "CREATE DATABASE IF NOT EXISTS `$DBnm`;";
-    while($tblNm = $stmtCheckTableListR->fetch_array(MYSQLI_NUM)) $createDBQ = $createDBQ."\nCREATE TABLE IF NOT EXISTS `$DBnm`.`$tblNm[0]` AS SELECT * FROM `eis_hrms_0demo`.`$tblNm[0]`;";
+    while ($tblNm = $stmtCheckTableListR->fetch_array(MYSQLI_NUM)) $createDBQ = $createDBQ . "\nCREATE TABLE IF NOT EXISTS `$DBnm`.`$tblNm[0]` AS SELECT * FROM `eis_hrms_0demo`.`$tblNm[0]`;";
     $link->multi_query($createDBQ);
   } else {
     $_SESSION["demoUsrID"] = null;
@@ -43,13 +43,12 @@ if ($mode === "LOGIN") {
   }
   mysqli_close($link);
 
-  
+
   $link = mysqli_connect('db', 'root', 'ic@0001', $DBnm);
-  if ($link->error)exit('error : '.json_encode($link->error));
+  if ($link->error) exit('error : ' . json_encode($link->error));
   $pridUsrMain = $_SESSION["user"]["prid"];
   $retArray = [];
   $UsrData  = [];
-  $key;
   $stmt     = $link->prepare("SELECT prid as `id`, title, fname, mname, lname, email as 'mail', post, contactno, `permissions`,
                                     address_c_houseno, address_c_area, address_c_city, address_c_state, address_c_country, address_c_pincode,
                                     address_p_houseno, address_p_area, address_p_city, address_p_state, address_p_country, address_p_pincode,current_salary,picture,createdAt
@@ -115,7 +114,76 @@ if ($mode === "LOGIN") {
   print_r(json_encode($_SESSION['userData']));
   exit($mode . "success" . json_encode($UsrData));
 } else {
-  // $link = mysqli_connect('db', 'root', 'ic@0001', $DBnm);
-  
+  $link = mysqli_connect('db', 'root', 'ic@0001', $DBnm);
+  // $link = mysqli_connect('db', 'root', 'ic@0001');
 }
-?>
+
+// if ($mode === "REGISTER") { // Add Users Who have Permissions
+//   $stmt = $link->prepare("INSERT into users(prid,title,fname,mname,lname,email,contactno,post,address_c_houseno,address_c_area,address_c_city,address_c_state,address_c_country,address_c_pincode,address_p_houseno,address_p_area,address_p_city,address_p_state,address_p_country,address_p_pincode) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+//   $stmt->bind_param(
+//     'issssssisssssisssssi',
+//     $data['prid'],
+//     $data["title"],
+//     $data["fname"],
+//     $data["mname"],
+//     $data["lname"],
+//     $data["email"],
+//     $data["contactno"],
+//     $data['post'],
+//     $data["address_c_houseno"],
+//     $data["address_c_area"],
+//     $data["address_c_city"],
+//     $data["address_c_state"],
+//     $data["address_c_country"],
+//     $data["address_c_pincode"],
+//     $data["address_p_houseno"],
+//     $data["address_p_area"],
+//     $data["address_p_city"],
+//     $data["address_p_state"],
+//     $data["address_p_country"],
+//     $data["address_p_pincode"]
+//   );
+//   $stmt->execute();
+//   if ($stmt->error) exit($stmt->error . " failure ");
+//   $stmtc = $link->prepare("INSERT INTO connections(prid1,prid2,connection)VALUES(?,?,'Boss')");
+//   $stmtc->bind_param('ii', $data['prid'], $data['boss']);
+//   $stmtc->execute();
+//   $newcon = $stmtc->insert_id;
+//   if ($stmtc->error) exit($stmtc->error . ' Failure');
+//   print_r($mode . " success" . json_encode($data));
+//   exit();
+// }
+
+if ($mode === "SALARYSLIP") { // Select Salart
+  $stmt = $link->prepare("SELECT prid,basic_salary,pf,medical,ta,da,hra,overtime,prepared_by,check_by,authorised_by,telephone_internet,bonus,house_rent,other FROM salaries where prid = ?");
+  $stmt->bind_param("i", $data['prid']);
+  $stmt->execute();
+  $res = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+  if ($stmt->error) exit(' Failure');
+  print_r($mode . " success" . json_encode($res));
+  exit();
+}
+
+if ($mode === "UPDATE_USERS") { // Users Can Be Updates
+  $ql = $link->prepare("UPDATE users set title=?,fname=?,mname=?,lname=?,email=?,contactno=?,address_c_houseno=?,address_c_area=?,address_c_city=?,address_c_state=?,address_c_country=?,address_c_pincode=?, address_p_houseno=?,address_p_area=?,address_p_city=?,address_p_state=?,address_p_country=?,address_p_pincode=? WHERE prid =?");
+  $ql->bind_param("sssssssssssisssssii", $data['title'], $data['fname'], $data['mname'], $data['lname'], $data['email'], $data['contactno'], $data['address_c_houseno'], $data['address_c_area'], $data['address_c_city'], $data['address_c_state'], $data['address_c_country'], $data['address_c_pincode'], $data['address_p_houseno'], $data['address_p_area'], $data['address_p_city'], $data['address_p_state'], $data['address_p_country'], $data['address_p_pincode'], $data['id']);
+  $ql->execute();
+  $idnew = $ql->insert_id;
+  if ($ql->error) exit(' failure');
+  $stmt = $link->prepare("SELECT * FROM users where id = $idnew");
+  $stmt->execute();
+  $newUsr = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+  if ($stmt->error) exit(' failure');
+  print_r($mode . " success" . json_encode($data));
+  exit();
+}
+
+if ($mode === "NOTIFICATION") { // Send Notification Who LoggedIn
+  $stmt = $link->prepare("SELECT id,`when`,why,what,whom FROM `notifications` WHERE whom = ?");
+  $stmt->bind_param("i", $data['whom']);
+  $stmt->execute();
+  $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+  if ($stmt->error) exit(' failure');
+  print_r($mode . " success" . json_encode($result));
+  exit;
+}
