76896d003699af897347411b72e613862caea03c
EIS LABS Pvt Ltd <shubhamm8989@gmail.com>
2020-02-14 16:04:47 +0530

    latest

--- l200207/frontend/src/app/server.ts
+++ l200207/frontend/src/app/server.ts
@@ -14,9 +14,9 @@ export class server {
       xhrFields: {
         withCredentials: true, 
       },
+      //crossDomain:true,
       data: data2Post,
-      success: (res, status, jqXHR) => {
-        this.getHeader(jqXHR,res=>{console.log(res)})
+      success: (res, status) => {
         var idxFailure = res.indexOf("failure");
         var idxSuccess = res.indexOf("success");
         var responsedecode;
@@ -31,20 +31,6 @@ export class server {
       cache: false,
       error: (error) => { console.log(error);next({ record: "Connection Fail With Server", mode: "serverissue", other: "" }); },
     });
-  }
-  getHeader = async (jqXHR, next) => {
-    var req =  jqXHR;
-    var data = new Object();
-    var headers = req.getAllResponseHeaders();
-    var aHeaders = headers.split('\n');
-    var i = 0;
-    for (i = 0; i < aHeaders.length; i++) {
-      var thisItem = aHeaders[i];
-      var key = thisItem.substring(0, thisItem.indexOf(':'));
-      var value = thisItem.substring(thisItem.indexOf(':') + 1);
-      data[key] = value;
-    }
-    next(data)
   }
   responseJsonconvertor = async (data, next) => { (data.success !== -1) ? next(JSON.parse(data.response.substr(data.success + 7))) : ""; }; // responseDecode
   requestJsonconvertor = async (data, next) => { (data !== undefined) ? next(JSON.parse(data)) : ""; };
