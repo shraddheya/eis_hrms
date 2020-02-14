a38ab09a98ff1a4c81ae3edcc0d4097d92bb3746
Madhuri@EIS LABS Pvt Ltd <madhuri@edeitic.com>
2020-02-14 16:00:53 +0530

    local changes

--- l200207/backend/preqsts.php
+++ l200207/backend/preqsts.php
@@ -9,7 +9,7 @@ $dur = array(
 
 $duration = ((($dur['days'] * 24 + $dur['hours']) * 60 + $dur['minutes']) * 60 + $dur['seconds'])*10000;
 header('Access-Control-Allow-Origin:localhost');
-header ('Access-Control-Allow-Credentials: true');
+// header ('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 error_reporting(E_ALL);
