46c546487b8bc792504245222ea5a7772f1eb05f
EIS LABS Pvt Ltd <shubhamm8989@gmail.com>
2020-02-24 17:42:00 +0530

    Adminpage feature

--- l200207/frontEndCleanShrad/src/app/admin/admin.component.ts
+++ l200207/frontEndCleanShrad/src/app/admin/admin.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { callUrl } from '../ajaxes';
-
+import $ from 'jquery';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -9,27 +9,125 @@ import { callUrl } from '../ajaxes';
 })
 export class AdminComponent implements OnInit {
   adminpagedata: any;
-  //exportadminpagedata: any = { Posts: [], Documents_accesslevels: [], Accesslevels: [] }
   exportadminpagedata: any;
   constructor(private router: Router) { }
 
   ngOnInit() {
-    callUrl({ mode: "ADMINDASHBOARD" }, (resp: any) => { 
-      this.adminpagedata = JSON.parse(resp);
+    callUrl({ mode: "ADMINDASHBOARD" }, (resp: any) => {
+      var resp = JSON.parse(resp);
+      resp.Posts.forEach(postel => { postel["data"] = postel.post });
+      resp.Accesslevels.forEach(dorrel => { dorrel["data"] = dorrel.name });
+      resp.Documents_accesslevels.forEach(docel => { docel["data"] = docel.name });
+      this.adminpagedata = resp;
       this.exportadminpagedata = [
-        {title:"Posts",icon:""},
-        {title:"Door Accesslevels",icon:""},
-        {title:"Document Accesslevels",icon:""},
+        {
+          title: "Posts",
+          clickFun: (_: any) => { this.showhideDefault('default_show_Posts') },
+          icon: "briefcase",
+          default: {
+            backicon: "backspace",
+            addicon: "plus-circle",
+            clickFun: (_: any) => { this.showhideDefault('default_hide_Posts') },
+            show: false,
+            data: {
+              clickFun: (_: any) => { this.clicked('hide_defaultdocaccess') },
+              record: ["Master"]
+            },
+          },
+          manual: {
+            title: "Posts",
+            removeicon: "minus-circle",
+            show: true,
+            data: resp.Posts,
+            inputfield: [{ name: "post", clickFun: (_: any) => { this.callFunction('manuallyadd_Posts') }, }]
+          },
+        },
+        {
+          title: "Dooraccess",
+          icon: "door-open",
+          manual: {
+            title: "Accesslevels",
+            removeicon: "minus-circle",
+            show: true,
+            data: resp.Accesslevels,
+            extra: "",
+            inputfield: [
+              { name: "name" },
+              { name: "inid" },
+              { name: "outid", clickFun: (_: any) => { this.callFunction('manuallyadd_Accesslevels') }, }]
+          },
+        },
+        {
+          title: "Docaccess",
+          clickFun: (_: any) => { this.showhideDefault('default_show_Docaccess') },
+          icon: "file-alt",
+          default: {
+            backicon: "backspace",
+            addicon: "plus-circle",
+            clickFun: (_: any) => { this.showhideDefault('default_hide_Docaccess') },
+            show: false,
+            data: {
+              clickFun: (_: any) => { this.clicked('hide_defaultdocaccess') },
+              record: [],
+            },
+          },
+          manual: {
+            removeicon: "minus-circle",
+            title: "Documents_accesslevels",
+            show: true,
+            data: resp.Documents_accesslevels,
+            inputfield: [{ name: "name", clickFun: (_: any) => { this.callFunction('manualyadd_Documents_accesslevels') } }]
+          },
+        },
       ]
     });
   }
 
+  showhideDefault(mode) {
+    this.exportadminpagedata.forEach(el => {
+      if (el.title == mode.substr(13)) {
+        var makemode = mode.substr(8).replace("_" + el.title, "");
+        if (makemode == "show") {
+          el.manual.show = false;
+          el.default.show = true;
+        }
+        if (makemode == "hide") {
+          el.manual.show = true;
+          el.default.show = false;
+        }
+      }
+    })
+  }
   clicked(mode) {
-   
+    switch (mode) {
+      case '':
+        return;
+    }
   }
-
   callFunction(mode) {
+    var requestMode = mode.substr(12).toUpperCase();
+    switch (mode) {
+      case 'manuallyadd_Posts':
+        var postinput = $("#fid_Posts_post").val()
+        if (postinput != "") callUrl({ mode: requestMode + "_ADD", data: JSON.stringify({ post: postinput }) }, (resp: any) => {
+          var resp = JSON.parse(resp);
+          resp["data"] = resp.post
+          this.exportadminpagedata.forEach(el => { if (el.manual.title == "Posts") this.adminpagedata.Posts.push(resp) })
+        })
+        return;
+      case 'manuallyadd_Accesslevels':
+        var sendObj = {};
+        ["name", "inid", 'outid'].forEach(el => { sendObj[el] = $("#fid_Accesslevels_" + el).val() })
+        Object.values(sendObj).forEach(chkel => {
+          (chkel == "") ? "" : "";
+        })
+        // callUrl({ mode: requestMode, data: JSON.stringify(sendObj) }, (resp: any) => {
 
+        // })
+        return;
+      case 'manualyadd_Documents_accesslevels':
+
+        return;
+    }
   }
-
 }
