c05d755c20506b34ab191275cfd56793f6cfa42a
EIS LABS Pvt Ltd <shubhamm8989@gmail.com>
2020-02-24 11:08:26 +0530

    adminpage

--- l200207/frontEndCleanShrad/src/app/admin/admin.component.ts
+++ l200207/frontEndCleanShrad/src/app/admin/admin.component.ts
@@ -2,32 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { callUrl } from '../ajaxes';
 
-var adminpagedata;
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  exportadminpagedata = { Posts: [], Documents_accesslevels: [], Accesslevels: [] }
-  exportdefaultdata = { Posts: [], Documents_accesslevels: [] }
+  adminpagedata: any;
+  //exportadminpagedata: any = { Posts: [], Documents_accesslevels: [], Accesslevels: [] }
+  exportadminpagedata: any;
   constructor(private router: Router) { }
 
-  clickedadmin(mode: string) {
-    switch (mode) {
-      case 'redirectHome':
-        this.router.navigate(['portal']);
-        break;
-      default:
-        break;
-    }
+  ngOnInit() {
+    callUrl({ mode: "ADMINDASHBOARD" }, (resp: any) => { 
+      this.adminpagedata = JSON.parse(resp);
+      this.exportadminpagedata = [
+        {title:"Posts",icon:""},
+        {title:"Door Accesslevels",icon:""},
+        {title:"Document Accesslevels",icon:""},
+      ]
+    });
   }
 
-  ngOnInit() {
-    callUrl({ mode: "ADMINDASHBOARD" }, (resp: any) => {
-      adminpagedata = JSON.parse(resp)
-      console.log(adminpagedata)
-    })
+  clicked(mode) {
+   
+  }
+
+  callFunction(mode) {
+
   }
 
 }
