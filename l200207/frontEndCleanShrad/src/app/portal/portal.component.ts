5102c9987c8a9da0431c3886b3b26da1472255dd
EIS LABS Pvt Ltd <shubhamm8989@gmail.com>
2020-02-25 15:54:55 +0530

    latestcode200225

--- l200207/frontEndCleanShrad/src/app/portal/portal.component.ts
+++ l200207/frontEndCleanShrad/src/app/portal/portal.component.ts
@@ -2,11 +2,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { callUrl } from '../ajaxes';
 import { ModalDirective } from 'angular-bootstrap-md';
-//import { runInThisContext } from 'vm';
+import * as moment from 'moment';
+import * as $ from 'jquery';
+import 'fullcalendar';
+
 declare var $jit: any;
-
 let rgraph: any;
-
 @Component({
   selector: 'app-portal',
   templateUrl: './portal.component.html',
@@ -27,7 +28,9 @@ export class PortalComponent implements OnInit {
   selOfTitle: any;
   selOfPermision: any;
   clickedUserid: any;
+  rootUserid: any;
   checkcardRecord: any;
+  editabel: any;
   userRecordbody = [
     {
       title: "Crud",
@@ -35,19 +38,19 @@ export class PortalComponent implements OnInit {
         name: "edit",
         icon: 'pencil-alt',
         clickFun: (_: any) => { this.clicked('editRecord') },
-        style: "nav-link waves-light text-dark",
+        style: "nav-link waves-light text-dark h3",
         show: true
       }, {
         name: "view",
         icon: 'eye',
         clickFun: (_: any) => { this.clicked('viewRecord') },
-        style: "nav-link waves-light text-dark",
+        style: "nav-link waves-light text-dark h3",
         show: false
       }, {
         name: "delete",
         icon: 'trash',
         clickFun: (_: any) => { this.callFunction('deleteRecord') },
-        style: "nav-link waves-light text-dark",
+        style: "nav-link waves-light text-dark h3",
         show: true
       }]
     }, {
@@ -56,24 +59,52 @@ export class PortalComponent implements OnInit {
         name: "cardbutton",
         icon: 'id-card',
         clickFun: (_: any) => { this.callFunction('cardallot') },
-        style: "waves-light rounded mb-0 h1 text-center z-depth-2 bg-dark text-white px-4 py-1",
+        style: "waves-light rounded mb-0 h3 text-center z-depth-2 bg-dark text-white px-4 py-1",
         show: true
       }, {
         name: "cardimage",
         img: '../../assets/images/cardscan2.gif',
         style: "px-5 py-4",
         show: false
+      }, {
+        name: "salaryslip",
+        title: "0",
+        clickFun: (_: any) => { this.callFunction('cardallot') },
+        style: "waves-light rounded mb-0 h3 text-center z-depth-2 bg-dark text-white px-4 py-1",
+        show: true
       },]
-    },
+    },{
+      title: "calendarmenu",
+      detail: [{
+        name: "form",
+        icon: 'calendar-times',
+        clickFun: (_: any) => { this.clicked('hidecalendar') },
+        style: "waves-light text-white bg-dark h3 z-depth-2 p-2",
+        show:false
+      },
+      {
+        name: "calandaricon",
+        icon: 'calendar-alt',
+        clickFun: (_: any) => { this.clicked('showcalendar') },
+        style: "waves-light text-white bg-dark h3 z-depth-2 p-2",
+        show: true
+      }]
+    }
   ];
-  userDetails = [
-    { title: "", detail: [], show: false },
-    { title: "Basic Detail", detail: [] , show: true},
-    { title: "Permanent Address", detail: [] , show: true},
-    { title: "Corresponding Address", detail: [] , show: true}
-  ]
+  userDetails = { data: [{ title: "Basic Detail", detail: [], show: true }, { title: "Permanent Address", detail: [], show: true }, { title: "Corresponding Address", detail: [], show: true }], show: true }
+  calendarUI = {show: false }
   constructor() { }
-
+  ngOnInit() {
+    this.initJIT();
+    callUrl({ mode: 'GETINITDATA' }, (resp: any) => {
+      resp = JSON.parse(resp)
+      this.accesslevels = resp.accesslevels;
+      this.posts = resp.posts;
+      this.users = resp.users;
+      this.createDataTree(this.users);
+      localStorage.setItem("checklogin", "true")
+    });
+  }
   addUserNode2GUI(user, mode, isPermanent = true) {
     switch (mode) {
       case 'addusers':
@@ -86,6 +117,17 @@ export class PortalComponent implements OnInit {
         this.users.forEach(function (item, index, object) { (item.id == user.prid) ? object.splice(index, 1) : "" });
         this.createDataTree(this.users);
         break;
+      case 'updateuser':
+        this.hierachyView.hide();
+        for (var i = 0; i < this.users.length; i++) {
+          if (this.users[i].id == user.id) {
+            this.users.splice(i, 1);
+            i--;
+          }
+        };
+        this.users.push(user);
+        this.createDataTree(this.users);
+        break;
     }
   }
   initJIT() {
@@ -96,7 +138,8 @@ export class PortalComponent implements OnInit {
       Node: { color: '#FFFFFF' },
       Edge: { color: '#C0C0C0', lineWidth: 1, spline: true },
       onCreateLabel: (domElement, node) => {
-        domElement.innerHTML = node.fname + + (node.mname ? node.mname + ' ' : '') + '  ' + node.lname;
+        //domElement.innerHTML = node.fname + + (node.mname ? node.mname + ' ' : '') + '  ' + node.lname;
+        domElement.innerHTML = node.fname + " " + node.lname;
         domElement.name = 'tooltip';
         domElement.className = 'show';
         const hoverdiv = document.createElement('div');
@@ -108,8 +151,8 @@ export class PortalComponent implements OnInit {
         const hovercontent = document.createElement('div');
         hovercontent.innerHTML = `
             <div id="mainrender"">
-              <div class="row">
-               <div class="col-2">UserID</div>
+              <div class="row" *ngFor="let hoverdata of array">
+               <div class="col-2">UserId</div>
                <div class="col-1">:</div>
                <div class="col-9">${node.id}</div>
               </div>
@@ -176,6 +219,7 @@ export class PortalComponent implements OnInit {
       (aData.boss) ? hashTable[aData.boss].children.push(hashTable[aData.id]) : dataTree.push(hashTable[aData.id]);
     });
     dataTree.forEach(obj => { this.showTree = obj; });
+    console.log(this.showTree)
     this.reloadGraph(this.showTree);
   }
   reloadGraph(d: any = null) {
@@ -190,14 +234,41 @@ export class PortalComponent implements OnInit {
     rgraph.fx.animate({ modes: ['polar'], duration: 2000 });
   }
   hierarchyViewdata(data) {
-    var recordObject = {}
+    this.clickedUserid = data.id;
+    this.rootUserid = data.boss;
+    this.userRecordbody.forEach(urel => {
+      urel.detail.forEach(udel => {
+        if (udel.name == "salaryslip") {
+          (data.current_salary == undefined) ? udel.title = "0" : udel.title = data.current_salary
+        }
+      })
+    })
+    this.userDetails.data.forEach(el => { el.detail = [] })
     this.users[0]["boss"] = data.boss
-    Object.keys(this.users[0]).forEach(el => { recordObject[el] = data[el] })
-    this.userDetails.forEach(clickel => {
-      if (clickel.title == "") {
-        clickel.detail.push({ title: "prid", value: data.id }, { title: "boss", value: data.boss })
+    this.userDetails.data.forEach(clickel => {
+      if (clickel.title == "Basic Detail") {
+        clickel.detail.push({ title: "name", record: [{ key: "title", value: data.title }, { key: "fname", value: data.fname }, { key: "mname", value: data.mname }, { key: "lname", value: data.lname },], icon: "user" })
+        clickel.detail.push({ title: "contactno", record: [{ key: "contactno", value: data.contactno }], icon: "mobile-alt" })
+        clickel.detail.push({ title: "email", record: [{ key: "email", value: data.email }], icon: "envelope" })
+      }
+      if (clickel.title == "Permanent Address") {
+        clickel.detail.push({ title: "address_p_houseno", record: [{ key: "address_p_houseno", value: data.address_p_houseno }], icon: "home" })
+        clickel.detail.push({ title: "address_p_area", record: [{ key: "address_p_area", value: data.address_p_area }], icon: "road" })
+        clickel.detail.push({ title: "address_p_city", record: [{ key: "address_p_city", value: data.address_p_city }], icon: "city" })
+        clickel.detail.push({ title: "address_p_state", record: [{ key: "address_p_state", value: data.address_p_state }], icon: "flag" })
+        clickel.detail.push({ title: "address_p_country", record: [{ key: "address_p_country", value: data.address_p_country }], icon: "globe" })
+        clickel.detail.push({ title: "address_p_pincode", record: [{ key: "address_p_pincode", value: data.address_p_pincode }], icon: "keyboard" })
+      }
+      if (clickel.title == "Corresponding Address") {
+        clickel.detail.push({ title: "address_c_houseno", record: [{ key: "address_c_houseno", value: data.address_c_houseno }], icon: "home" })
+        clickel.detail.push({ title: "address_c_area", record: [{ key: "address_c_area", value: data.address_c_area }], icon: "road" })
+        clickel.detail.push({ title: "address_c_city", record: [{ key: "address_c_city", value: data.address_c_city }], icon: "city" })
+        clickel.detail.push({ title: "address_c_state", record: [{ key: "address_c_state", value: data.address_c_state }], icon: "flag" })
+        clickel.detail.push({ title: "address_c_country", record: [{ key: "address_c_country", value: data.address_c_country }], icon: "globe" })
+        clickel.detail.push({ title: "address_c_pincode", record: [{ key: "address_c_pincode", value: data.address_c_pincode }], icon: "keyboard" })
       }
     })
+    this.populateCalendarAttendance(data.id)
     this.hierachyView.show();
   }
   clicked(mode) {
@@ -219,6 +290,7 @@ export class PortalComponent implements OnInit {
             (carel.name == "delete") ? carel.show = true : "";
           }) : "";
         })
+        this.editabel = true;
         break;
       case 'viewRecord':
         this.userRecordbody.forEach(parel => {
@@ -227,9 +299,18 @@ export class PortalComponent implements OnInit {
             (carel.name == "delete") ? carel.show = true : "";
           }) : "";
         })
+        this.editabel = false;
         break;
       case 'allotimgLoad':
         this.userRecordbody.forEach(parel => { (parel.title == "Buttons") ? parel.detail.forEach(carel => { (carel.name == "cardimage") ? (this.checkcardRecord.tagid == "") ? carel.show = true : carel.show = false : "" }) : ""; })
+        break;
+      case 'showcalendar':
+        this.userDetails.show = false;
+        this.calendarUI.show = true;
+        break;
+      case 'hidecalendar':
+        this.userDetails.show = true;
+        this.calendarUI.show = false;
         break;
       default:
         break;
@@ -261,6 +342,18 @@ export class PortalComponent implements OnInit {
           this.addUserNode2GUI(resp, "addusers");
         })
         break;
+      case 'updateData':
+        var updObj = {};
+        $(".updatedetail").each((_, el) => { updObj[el.id] = $(el).html() });
+        updObj["prid"] = this.clickedUserid;
+        updObj["boss"] = this.rootUserid;
+        callUrl({ mode: "UPDATE_USERS", data: JSON.stringify(updObj) }, (resp: any) => {
+          resp = JSON.parse(resp);
+          resp.id = resp.prid;
+          delete resp["prid"];
+          this.addUserNode2GUI(resp, "updateuser");
+        })
+        break;
     }
   }
   objectToarray(input, mode) {
@@ -273,15 +366,16 @@ export class PortalComponent implements OnInit {
         break;
     }
   }
-  ngOnInit() {
-    this.initJIT();
-    callUrl({ mode: 'GETINITDATA' }, (resp: any) => {
-      resp = JSON.parse(resp)
-      this.accesslevels = resp.accesslevels;
-      this.posts = resp.posts;
-      this.users = resp.users;
-      this.createDataTree(this.users);
-      localStorage.setItem("checklogin", "true")
-    });
+  populateCalendarAttendance(prid, dataInterest = moment()){
+    console.log(prid, dataInterest)
+    $('#calendar').fullCalendar({
+      defaultDate: moment().format('YYYY-MM-DD'),
+      editable: true,
+      eventLimit: false,
+      viewRender: (view, event) => {
+        var moments = $('#calendar').fullCalendar('getDate');
+        var data = moments.format();
+      }
+    })
   }
 }
