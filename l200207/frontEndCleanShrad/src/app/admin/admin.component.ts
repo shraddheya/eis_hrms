b309000535d74aab0f0181932a4e3e8ba818463e
EIS LABS Pvt Ltd <shubhamm8989@gmail.com>
2020-02-27 15:57:26 +0530

    adminpage

--- l200207/frontEndCleanShrad/src/app/admin/admin.component.ts
+++ l200207/frontEndCleanShrad/src/app/admin/admin.component.ts
@@ -15,9 +15,10 @@ export class AdminComponent implements OnInit {
   ngOnInit() {
     callUrl({ mode: "ADMINDASHBOARD" }, (resp: any) => {
       var resp = JSON.parse(resp);
-      resp.Posts.forEach(postel => { postel["data"] = postel.post });
-      resp.Accesslevels.forEach(dorrel => { dorrel["data"] = dorrel.name });
-      resp.Documents_accesslevels.forEach(docel => { docel["data"] = docel.name });
+      // console.log(resp)
+      // resp.Posts.forEach(postel => { postel["data"] = postel.post });
+      // resp.Accesslevels.forEach(dorrel => { dorrel["data"] = dorrel.name });
+      // resp.Documents_accesslevels.forEach(docel => { docel["data"] = docel.name });
       this.adminpagedata = resp;
       this.exportadminpagedata = [
         {
