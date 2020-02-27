a10879e072c5b11974e1984c26044b7c5d7512f8
Shraddheya <shraddheya.shrivastava@gamil.com>
2020-02-27 14:55:24 +0530

    local commits

--- l200207/frontEndCleanShrad/src/app/topbar/topbar.component.ts
+++ l200207/frontEndCleanShrad/src/app/topbar/topbar.component.ts
@@ -1,10 +1,12 @@
+// tslint:disable: no-shadowed-variable
+// tslint:disable: variable-name
 // tslint:disable: max-line-length
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ModalDirective } from "angular-bootstrap-md";
+import { ModalDirective } from 'angular-bootstrap-md';
 import { callUrl } from '../ajaxes';
 import { Router } from '@angular/router';
 import $ from 'jquery';
-var check;
+let check: any;
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
@@ -14,7 +16,8 @@ export class TopbarComponent implements OnInit {
   email: string;
   password: string;
   constructor(private router: Router) { }
-  @ViewChild('loginmodal', { static: true }) loginmodal: ModalDirective
+  @ViewChild('loginmodal', { static: true })
+  loginmodal: ModalDirective;
   topBarElements = [
     {
       name: 'Features',
@@ -33,7 +36,7 @@ export class TopbarComponent implements OnInit {
       icon: 'sign-in-alt',
       class: 'nav-link waves-light',
       clickFun: (_: any) => { this.clicked('showLoginModel'); },
-      //clickFun: (_: any) => {this.callFunction('LOGIN'); },
+      // clickFun: (_: any) => {this.callFunction('LOGIN'); },
       show: true
     }, {
       name: 'Login',
@@ -46,9 +49,9 @@ export class TopbarComponent implements OnInit {
       class: 'dropdown-toggle nav-link waves-light',
       name: 'Menue',
       icon: 'ellipsis-v',
-      //clickFun: (_: any) => { this.clicked('menues'); },
+      // clickFun: (_: any) => { this.clicked('menues'); },
       show: false
-    },//Backtoportal
+    }, // Backtoportal
     {
       class: 'nav-link waves-light',
       name: 'Backtoportal',
@@ -58,17 +61,17 @@ export class TopbarComponent implements OnInit {
     },
   ];
   portalmenuDropdown = [
-    { icon: "bell", clickFun: (_: any) => { this.clicked('notification') }, show: true },
-    { icon: "user-shield", clickFun: (_: any) => { this.clicked('adminpanel') }, show: true },
-    { icon: "cog", clickFun: (_: any) => { this.clicked('setting') }, show: true },
-    { icon: "sign-out-alt", clickFun: (_: any) => { this.clicked('logout') }, show: true },
-  ]
+    { icon: 'bell', clickFun: (_: any) => { this.clicked('notification'); }, show: true },
+    { icon: 'user-shield', clickFun: (_: any) => { this.clicked('adminpanel'); }, show: true },
+    { icon: 'cog', clickFun: (_: any) => { this.clicked('setting'); }, show: true },
+    { icon: 'sign-out-alt', clickFun: (_: any) => { this.clicked('logout'); }, show: true },
+  ];
 
   callFunction(mode: string) {
     switch (mode) {
       case 'LOGIN':
         callUrl({ mode, data: JSON.stringify({ email: this.email, password: this.password }) }, (_: any) => {
-          this.router.navigate(['portal'])
+          this.router.navigate(['portal']);
           check = true;
         });
         break;
@@ -81,49 +84,49 @@ export class TopbarComponent implements OnInit {
     switch (mode) {
       case 'showLoginModel':
         this.loginmodal.show();
-        $(".modal-backdrop").hide();
+        $('.modal-backdrop').hide();
         break;
       case 'hideLoginModel':
         this.loginmodal.hide();
         break;
       case 'menues':
-        console.log("Admin page");
+        console.log('Admin page');
         break;
       case 'adminpanel':
-        this.router.navigate(['admin'])
+        this.router.navigate(['admin']);
         break;
       case 'backtoportal':
-        this.router.navigate(['portal'])
+        this.router.navigate(['portal']);
         break;
       case 'logout':
         this.router.navigate(['']);
-        localStorage.setItem("checklogin","false")
+        localStorage.setItem('checklogin', 'false');
         break;
       default:
         break;
     }
   }
-  passwordView(format, input, showhide_event) {
-    $(showhide_event).attr("type", format);
-    $(".show" + format).hide();
-    $(".show" + input).show();
+  passwordView(format: any, input: any, showhide_event: any) {
+    $(showhide_event).attr('type', format);
+    $('.show' + format).hide();
+    $('.show' + input).show();
   }
 
   ngOnInit() {
-    let checkurl: string = window.location.href.replace("http://localhost:4200/", "")
-    if (checkurl == "portal" || checkurl == "admin") {
-      var check = localStorage.getItem("checklogin")
-      if ((check == null) || (check == "false")) {
-        this.router.navigate([''])
+    const checkurl: string = window.location.href.replace('http:// localhost:4200/', '');
+    if (checkurl === 'portal' || checkurl === 'admin') {
+      const check = localStorage.getItem('checklogin');
+      if ((check === null) || (check === 'false')) {
+        this.router.navigate(['']);
       }
-      if (check == "true") {
-        this.router.navigate([checkurl])
+      if (check === 'true') {
+        this.router.navigate([checkurl]);
         this.topBarElements.forEach(el => {
-          (checkurl == "portal")?(el.name == "Menue") ? el.show = true : el.show = false:"";
-          (checkurl == "admin")?(el.name == "Backtoportal") ? el.show = true : el.show = false:"";
+          (checkurl === 'portal') ? (el.name === 'Menue') ? el.show = true : el.show = false : '';
+          (checkurl === 'admin') ? (el.name === 'Backtoportal') ? el.show = true : el.show = false : '';
         });
       }
     }
-    //((checkurl == "potal")||(checkurl == "admin"))?(checklogin == false)?this.router.navigate(['']):this.router.navigate([checklogin]):"";
+    // ((checkurl == "potal")||(checkurl == "admin"))?(checklogin == false)?this.router.navigate(['']):this.router.navigate([checklogin]):"";
   }
 }
